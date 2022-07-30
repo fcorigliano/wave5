9b5036c700312733a2b0e2fccff7c87e3c904bfb
Facundo Corigliano <facundo.corigliano@mercadolibre.com>
2022-07-30 01:37:02 -0300

    clase2 node modules

--- express-clase2/ejercitacion/routes/products.js
+++ express-clase2/ejercitacion/routes/products.js
@@ -7,40 +7,15 @@ const router = express.Router();
 router.get('/products', (req, res) => {
     res.json(products);
 })
-
 //2. Obtener un producto específico mediante su ID “/api/products/:id”
 router.get('/products/:id', (req, res) => {
-    const id = req.params.id;
-    const product = products.filter(x => x.id == id);
-    res.json(product);
+    const { id } = req.params;
+    
 })
-
 //3. Agregar un nuevo producto “/api/products”
-router.post('/products/add', (req, res) => {
-    const name = req.body.name;
-    const colors = req.body.colors;
-    const price = req.body.price;
-    const quantity = req.body.quantity;
-    const obj = {name, colors, price, quantity};
-
-    products.push(obj);
-    res.send(`Se agregó el producto ${name}`);
-})
 
 //4. Cambiar alguna propiedad de un producto en particular ( Puede ser name, price, quantity o el que desees ) “/api/products/:id”,
-router.put('/products/:id/:name', (req, res) => {
-    const { id, name } = req.params;
-    const result = products.find(product => product.id === parseInt(id));
-
-    result.name = name;
-    res.json(result);
-})
 
 //5. Eliminar un producto mediante su ID “/api/products/:id”,
-router.delete('/products/:id', (req, res) => {
-    const id = req.params.id;
-    const product = products.filter(x => x.id != id);
-    res.json(product);
-})
 
 module.exports = { router, products };
